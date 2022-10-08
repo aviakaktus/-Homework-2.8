2e36ce8e463f739bf438c51d3498f28e2e7b4567
Vladimr <mashkovvv11@gmail.com>
2022-10-08 18:34:04 +0500

    Initial commit

---
+++ src/test/java/pro/sky/java/course2/homework_2_8/Homework28ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.homework_2_8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework28ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
